bacf664d09577e631c0fdefcb57c97be9bc8376a
Ayush Saklani <saklaniayush99@gmail.com>
2021-05-30 22:31:52 +0530

    Added Activity methods to CameraActivity.java

--- app/src/main/java/com/ayushsaklani/multidoc/camera/CameraActivity.java
+++ app/src/main/java/com/ayushsaklani/multidoc/camera/CameraActivity.java
@@ -1,19 +1,17 @@
 package com.ayushsaklani.multidoc.camera;
 
+import android.os.Bundle;
+import android.util.Size;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.AspectRatio;
 import androidx.camera.core.CameraSelector;
 import androidx.camera.core.Preview;
-import androidx.camera.core.impl.PreviewConfig;
-import androidx.camera.core.internal.ThreadConfig;
 import androidx.camera.lifecycle.ProcessCameraProvider;
 import androidx.camera.view.PreviewView;
 import androidx.core.content.ContextCompat;
 import androidx.lifecycle.LifecycleOwner;
-
-import android.os.Bundle;
-import android.util.Size;
-import android.view.WindowManager;
 
 import com.ayushsaklani.multidoc.R;
 import com.google.common.util.concurrent.ListenableFuture;
@@ -24,6 +22,7 @@ public class CameraActivity extends AppCompatActivity {
 
     private PreviewView previewView;
     private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
+    private static final Size DESIRED_PREVIEW_SIZE = new Size(480, 480);
 
 
 
@@ -31,19 +30,9 @@ public class CameraActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         previewView = findViewById(R.id.view_frame);
         cameraProviderFuture = ProcessCameraProvider.getInstance(this);
 
-        cameraProviderFuture.addListener((Runnable) () -> {
-            try{
-                ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
-                bindImageAnalysis(cameraProvider);
-            } catch (InterruptedException | ExecutionException e) {
-                e.printStackTrace();
-            }
-
-        }, ContextCompat.getMainExecutor(this));
 
     }
 
@@ -51,7 +40,7 @@ public class CameraActivity extends AppCompatActivity {
         previewView.setImplementationMode(PreviewView.ImplementationMode.COMPATIBLE);
 
         Preview preview = new Preview.Builder()
-                .setTargetResolution(new Size(640,640))
+                .setTargetResolution(DESIRED_PREVIEW_SIZE)
                 .build();
         CameraSelector cameraSelector = new CameraSelector.Builder()
                 .requireLensFacing(CameraSelector.LENS_FACING_BACK)
@@ -66,5 +55,40 @@ public class CameraActivity extends AppCompatActivity {
     public void onBackPressed() {
         super.onBackPressed();
         finish();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        cameraProviderFuture.addListener((Runnable) () -> {
+            try{
+                ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+                bindImageAnalysis(cameraProvider);
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
+
+        }, ContextCompat.getMainExecutor(this));
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        cameraProviderFuture.cancel(true);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(!cameraProviderFuture.isCancelled()){
+            cameraProviderFuture.cancel(true);
+
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }
